5602d463bd61b4eb30323733610bc1bae268e4d7
Erika Alcântara <erika.alcantara@edu.unifil.br>
2021-08-21 17:46:29 -0300

    Sidebar

--- bissecao.py
+++ bissecao.py
@@ -5,20 +5,21 @@ import pandas as pd
 
 
 st.title('Aplicação para o Método da Bissecção')
-st.write('Dada a função: X^5+X^4+X^3+X^2+X+C')
-st.write('Insira os valores da função:')
+st.sidebar.title('Método da Bissecção')
+st.sidebar.write('Dada a função: ')
+st.sidebar.latex(r"""a xˆ5 + b xˆ4 + c xˆ3 + d xˆ2 + ex + c""")
+st.sidebar.write('Insira os valores para X: ')
 
-num_input1 = st.number_input("Xˆ5: ", min_value = -100, max_value=100, value=0, step=1) 
-num_input2 = st.number_input("Xˆ4: ", min_value = -100, max_value=100, value=0, step=1) 
-num_input3 = st.number_input("Xˆ3: ", min_value = -100, max_value=100, value=0, step=1) 
-num_input4 = st.number_input("Xˆ2: ", min_value = -100, max_value=100, value=0, step=1) 
-num_input5 = st.number_input("X: ", min_value = -100, max_value=100, value=0, step=1) 
-num_input6 = st.number_input("C: ", min_value = -100, max_value=100, value=0, step=1) 
-num_input7 = st.number_input("Epsilon: ", min_value = -100, max_value=100, value=0, step=1) 
-st.button("Entra")
+x5 = st.sidebar.number_input("Xˆ5: ", min_value = -100, max_value=100, value=0, step=1) 
+x4 = st.sidebar.number_input("Xˆ4: ", min_value = -100, max_value=100, value=0, step=1) 
+x3 = st.sidebar.number_input("Xˆ3: ", min_value = -100, max_value=100, value=0, step=1) 
+x2 = st.sidebar.number_input("Xˆ2: ", min_value = -100, max_value=100, value=0, step=1) 
+x1 = st.sidebar.number_input("X: ", min_value = -100, max_value=100, value=0, step=1) 
+c= st.sidebar.number_input("C: ", min_value = -100, max_value=100, value=0, step=1) 
+ep = st.sidebar.number_input("Epsilon: ", min_value = -100, max_value=100, value=0, step=1) 
 
 
-d = {'f(x)': [1,2,3,4,5], 'g(x)': [5,4,3,2,1], 'h(x)': [6,7,8,9,10]}
+d = {'f(x)': [x5,x4,x3,x2,c], 'g(x)': [5,4,3,2,1], 'h(x)': [6,7,8,9,10]}
 
 progress_bar = st.sidebar.progress(0)
 status_text = st.sidebar.empty()
